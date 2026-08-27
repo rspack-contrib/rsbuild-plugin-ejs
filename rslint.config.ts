443d974bd64fb711868330f504ac3beba28d6794
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-08-27 18:55:50 +0800

    chore(deps): upgrade Rslint to v0.8.2 (#67)

--- rslint.config.ts
+++ rslint.config.ts
@@ -1,12 +1,3 @@
 import { defineConfig, js, ts } from '@rslint/core';
 
-export default defineConfig([
-  js.configs.recommended,
-  ts.configs.recommended,
-  {
-    files: ['playground/**/*', 'test/**/*'],
-    rules: {
-      'no-undef': 'off',
-    },
-  },
-]);
+export default defineConfig([js.configs.recommended, ts.configs.recommended]);
